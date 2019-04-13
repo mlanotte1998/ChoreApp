4248c9fd2239c31f0e3eee9d0d9ea66e04bc9e4e
mlanotte1998 <mlanotte1998@gmail.com>
2019-04-13 10:58:31 -0400

    starting app

---
+++ ChoreApp/ChoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChoreApp
+//
+//  Created by Michael Lanotte on 4/13/19.
+//  Copyright © 2019 Michael Lanotte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
